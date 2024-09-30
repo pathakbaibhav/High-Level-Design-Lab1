68a3a7c4d84e30e0b142318fe6b8188e2ab979ac
robbiedantonio <robbie.dantonio@gmail.com>
2024-09-30 12:54:13 -0400

    Issue #4 - implemented matrix_snr() to calculate the SNR between two matrices

--- gemm_fp/testbench.cpp
+++ gemm_fp/testbench.cpp
@@ -13,6 +13,26 @@ int roundup(float fp_number)
 	return	fx_number;
 }
 
+float matrix_snr(float* uut_gemm_cout, float* tst_matrix_Cout, int M, int N) {
+	// ddif = uut_gemm_cout – tst_matrix_Cout;
+	// disp([‘SNR is’, num2str(10*log10(sum(tst_matrix_Cout(:).^2)/sum(ddiff(:).^2))), ‘dB’]);
+
+	float signal_power = 0.0;	// Signal strength 
+    float noise_power = 0.0;	// Noise strength
+    int length = M*N;
+
+    for (int i = 0; i < length; i++) {
+        signal_power += tst_matrix_Cout[i] * tst_matrix_Cout[i];      // Square signal
+        float diff = uut_gemm_cout[i] - tst_matrix_Cout[i];           // Calculate noise
+        noise_power += diff * diff;                                   // Square noise
+    }
+
+    // SNR in dB
+    float snr = 10 * std::log10f(signal_power / noise_power);
+
+    return snr;
+}
+
 int main(int argc, char* argv[])
 {
 	// how to compute 0.213 * 500 in fixed point?
@@ -34,9 +54,6 @@ int main(int argc, char* argv[])
 	printf("%.10f\n",result);
 	*/
 
-
-	printf("%.10f\n", tst_matrix_A[0]);
-
 	float ALPHA=1.0;
 	float BETA=0.0;
 
@@ -47,9 +64,11 @@ int main(int argc, char* argv[])
 	// Call gemm_cpu
 	cpu_gemm_nn(0, 0, tst_dim_M, tst_dim_N, tst_dim_K, ALPHA, tst_matrix_A, lda, tst_matrix_B, ldb, BETA, tst_matrix_Cin, ldc);
 
-	for(unsigned int i=0; i<tst_dim_N; i=i+40){
-		printf("gemm output: %.10f, actual: %.10f\n", tst_matrix_Cin[i], tst_matrix_Cout[i]);
-	}
+	// for(unsigned int i=0; i<tst_dim_N; i=i+40){
+	// 	printf("gemm output: %.10f, actual: %.10f\n", tst_matrix_Cin[i], tst_matrix_Cout[i]);
+	// }
+
+	printf("SNR is %.10f dB\n", matrix_snr(tst_matrix_Cin, tst_matrix_Cout, tst_dim_M, tst_dim_N));
 
 
 }
