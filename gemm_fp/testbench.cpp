7c7da8d49c917841b728238a249e659e84558a4d
robbiedantonio <robbie.dantonio@gmail.com>
2024-09-30 12:09:43 -0400

    Issue #4 – Testing gemm and comparing with groundtruth cout

--- gemm_fp/testbench.cpp
+++ gemm_fp/testbench.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
+#include "gemm_cpu_fp.h"
+#include "testData.h"
+
 int roundup(float fp_number)
 {
 	int	fx_number	=	(int)fp_number;
@@ -13,7 +16,7 @@ int roundup(float fp_number)
 int main(int argc, char* argv[])
 {
 	// how to compute 0.213 * 500 in fixed point?
-	
+	/*
 	float FloatPointValue = 0.213;
 	int scale = 16;
 	
@@ -29,4 +32,24 @@ int main(int argc, char* argv[])
 	// printing result
 	printf("%.10f\n",FloatPointValue*500);
 	printf("%.10f\n",result);
+	*/
+
+
+	printf("%.10f\n", tst_matrix_A[0]);
+
+	float ALPHA=1.0;
+	float BETA=0.0;
+
+	int lda = tst_dim_K;
+    int ldb = tst_dim_N;
+    int ldc = tst_dim_N;
+
+	// Call gemm_cpu
+	cpu_gemm_nn(0, 0, tst_dim_M, tst_dim_N, tst_dim_K, ALPHA, tst_matrix_A, lda, tst_matrix_B, ldb, BETA, tst_matrix_Cin, ldc);
+
+	for(unsigned int i=0; i<tst_dim_N; i=i+40){
+		printf("gemm output: %.10f, actual: %.10f\n", tst_matrix_Cin[i], tst_matrix_Cout[i]);
+	}
+
+
 }
